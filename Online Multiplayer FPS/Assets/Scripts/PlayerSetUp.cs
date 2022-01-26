0aab18435787d2cc9c76c12fe1f95485afdb0961
Alexander <alexandergregoremmanuel.sanchez@benilde.edu.ph>
2022-01-26 22:55:24 +0800

    Module 1

---
+++ Online Multiplayer FPS/Assets/Scripts/PlayerSetUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class PlayerSetUp : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private GameObject cameraPOV;
+
+    [SerializeField]
+    TextMeshProUGUI playerNameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            transform.GetComponent<MovementController>().enabled = true;
+            cameraPOV.GetComponent<Camera>().enabled = true;
+        }
+        else
+        {
+            transform.GetComponent<MovementController>().enabled = false;
+            cameraPOV.GetComponent<Camera>().enabled = false;
+        }
+
+        playerNameText.text = photonView.Owner.NickName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
